f87780364569cf109de0b597e3729189d0d1bed5
Daniel Smith <dannmasta@gmail.com>
2017-08-17 00:00:44 -0700

    add base package

---
+++ index.js
@@ -0,0 +1,105 @@
+const path = require('path');
+const fs = require('fs');
+
+// primitive types
+const types = {
+    'true': true,
+    'false': false,
+    'null': null,
+    'undefined': undefined,
+    'NaN': NaN
+};
+
+// https://stackoverflow.com/a/1830844/2180385
+function isNumeric(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
+// get env variable, converts to native type
+function get(key){
+
+    let val = process.env[key];
+
+    return val in types ? types[val] : isNumeric(val) ? parseFloat(val) : val;
+
+};
+
+// sets environment variable if it does not exist already
+// env variables are stringified when set
+function set(key, val){
+    return process.env[key] = (process.env[key] === undefined ? val : process.env[key]);
+};
+
+function env(key, val){
+
+    // handle get
+    if(typeof key === 'string' && val === undefined){
+        return get(key);
+    }
+
+    // handle setting single values
+    if(arguments.length === 2){
+        return set(key, val);
+    }
+
+    // handle setting objects
+    if(key && typeof key === 'object' && !(key instanceof Array)){
+        for(let i in key){
+            set(i, key[i]);
+        }
+    }
+
+    return process.env;
+
+};
+
+// attempt to load env configuration files
+function init(){
+
+    // load and parse .env file
+    ['./.env'].map(file => {
+
+        try {
+
+            fs.readFileSync(path.resolve(file), 'utf8').split(/\r\n|\r|\n/g).map(str => {
+
+                if(str){
+                    set.apply(null, str.split('='));
+                }
+
+            });
+
+        } catch(err) {
+
+            if(env('DEBUG')){
+                console.log(`Env: ${err.message}`);
+            }
+
+        }
+
+    });
+
+    // load .js and .json files
+    ['./env', './config/env'].map(file => {
+
+        try {
+
+            env(require(path.resolve(file)));
+
+        } catch(err) {
+
+            if(env('DEBUG')){
+                console.log(`Env: ${err.message}`);
+            }
+
+        }
+
+    });
+
+    set('DEV', /development|undefined/.test(get('NODE_ENV')));
+
+};
+
+init();
+
+module.exports = env;

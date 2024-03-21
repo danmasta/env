80bb1c7cb5c8abe55fda47c2e612b7367109b32d
Daniel Smith <dannmasta@gmail.com>
2024-03-20 21:23:26 -0700

    update tests setup

--- tests/_setup.js
+++ tests/_setup.js
@@ -1,4 +1,3 @@
-const chai = require('chai');
 const path = require('path');
 const fs = require('fs');
 const Env = require('../lib/env');
@@ -8,12 +7,14 @@ const envFilePath = path.resolve(__dirname, './.env');
 const envFileContents = fs.readFileSync(envFilePath).toString();
 
 beforeEach(() => {
-    global.assert = chai.assert;
-    global.expect = chai.expect;
-    global.should = chai.should();
-    global.Env = Env;
-    global.env = new Env();
-    global.envFilePath = envFilePath;
-    global.envFileContents = envFileContents;
-    global.util = util;
+    return import('chai').then(chai => {
+        global.assert = chai.assert;
+        global.expect = chai.expect;
+        global.should = chai.should();
+        global.Env = Env;
+        global.env = new Env();
+        global.envFilePath = envFilePath;
+        global.envFileContents = envFileContents;
+        global.util = util;
+    });
 });

4baa2925e35a9c270eb86dda82898b49a66b9d47
Daniel Smith <dannmasta@gmail.com>
2024-07-10 17:06:54 -0700

    add rollup config

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import resolve from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
+
+export default [
+    {
+        input: [
+            'index.cjs',
+            'lib/constants.js',
+            'lib/env.js',
+            'lib/util.js',
+            'lib/vault-worker.js',
+            'lib/vault.js'
+        ],
+        output: {
+            dir: 'dist/cjs',
+            format: 'cjs',
+            sourcemap: false,
+            strict: false,
+            preserveModules: true,
+            exports: 'auto',
+            entryFileNames: '[name].[format]',
+            esModule: false
+        },
+        plugins: [
+            resolve(),
+            replace({
+                'vault-worker.js': 'vault-worker.cjs'
+            })
+        ],
+        external: [
+            '@danmasta/lo',
+            '@danmasta/lo/errors',
+            'minimist'
+        ]
+    }
+];
